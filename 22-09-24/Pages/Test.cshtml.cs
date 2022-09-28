4aff3bbc7330c67f2bdc765145850ca8e5ae8688
PMBytner <pawel.m.bytner@gmail.com>
2022-09-28 17:29:53 +0200

    Dodaj pliki projektów.

---
+++ 22-09-24/Pages/Test.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _22_09_24.Pages
+{
+    public class TestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
